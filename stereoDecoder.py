dd1a87768b0622b3d8568cada9d0a56f6820e753
Aron Goldberg <arongoldberg4@gmail.com>
2022-02-15 22:21:14 -0500

    created outline for stereo decoder class

---
+++ stereoDecoder.py
@@ -0,0 +1,16 @@
+
+################### AUDIO OUTPUT/STEREO DECODER SECTION ################### 
+
+# Author: Developed and maintained by Aron Goldberg
+# Creation Date: 2/15/2022
+# Last Updated: 2/15/2022
+# License: MIT License 2022
+# Further Description:
+#   Stub function section for the Adafruit I2S Stereo Decoder - UDA1334A
+#   This section was written in VS Code and tested on a Raspberry Pi Zero
+
+
+# TODO:
+#       write stub functions and classes
+
+class StereoDecoder:
