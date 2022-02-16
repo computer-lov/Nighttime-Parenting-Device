190867e6ed2a2cf9d122e42ca6d84d6f988b6bd5
Your Name <you@example.com>
2022-02-15 21:41:48 -0500

    Corrected setup.py and new micCircuit.py file

--- setup.py
+++ setup.py
@@ -1,31 +1,11 @@
+from setuptools import setup
 
-
-################### MICROPHONE SOUND LEVEL CIRCUIT SECTION ################### 
-
-# Author: Developed and maintained by Andrew P. Mayer
-# Creation Date: 2/15/2022
-# Last Updated: 2/15/2022
-# License: MIT License 2022
-# Further Description:
-#   Stub function section for the CMEJ-9745-37-P Electric Condensor Microphone
-#   This section was written in VS Code and tested on a Raspberry Pi Zero
-
-# TODO:
-#       create stub functions and classes
-#       write stub functions and classes
-
-
-
-class micCircuit: 
-
-    def __init__(self):
-        pass;
-
-    def readAnalogVal(self):
-        pass;
-
-    def getA2C(self):
-        pass;
-
-
-
+setup(name='micCircuit', 
+      version='1.0',
+      description='Library for baby monitoring subsystem',
+      author='Andrew Paul Mayer',
+      author_email='apm532@nyu.edu',
+      url='git@github.com:computer-lov/Nighttime-Parenting-Device.git',
+      install_requires=['RPi.GPIO'],
+      py_modules=['micCircuit']
+      )
