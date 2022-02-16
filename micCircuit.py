96094468fc1853d5d294244dd976daf67963cd7b
Your Name <you@example.com>
2022-02-16 10:23:10 -0500

    Filled in micCircuit class methods

--- micCircuit.py
+++ micCircuit.py
@@ -1,3 +1,4 @@
+import spidev
 
 ################### MICROPHONE SOUND LEVEL CIRCUIT SECTION ################### 
 
@@ -9,29 +10,38 @@
 #   Stub function section for the CMEJ-9745-37-P Electric Condensor Microphone
 #   This section was written in VS Code and tested on a Raspberry Pi Zero
 
-
-# TODO:
-#       write stub functions and classes
-
-
-
 class micCircuit: 
 
     # initializes micCircuit class
     def __init__(self):
-        pass
+        # these numbers may need to be changed
+        self.ADC_CH0 = 0b01101000
 
-    # reads in analog value and returns it
-    def readAnalogVal(self):
-        pass
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 1)
+        self.spi.mode = 0b00
+        self.spi.max_speed_hz = 1200000
 
-    # converts given analog value to digital value
-    def atod(self, aVal):
-        pass
+    # reads in digital value and returns it
+    def getDigitalVal(self):
+        # Read from CH0
+        readBytes = self.spi.xfer2([self.ADC_CH0, 0x00])
+        # obtain digital value
+        dVal = (((readBytes[0] & 0b11) << 8) | readBytes[1])
+        return dVal
 
+    # converts digital value to analog value
+    def getAnalogVal(self):
+        # get digital value
+        dVal = self.getDigitalVal()
+        aVal = (dVal/1024)*3.3
+        return aVal
+        
     # returns true if curr analog value is greater than threshold false otherwise
     def trigger(self, thresholdVal):
-        pass
+        res = True if (self.getAnalogVal() > thresholdVal) else False
+        return res
+        
 
 
 
