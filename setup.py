0e389f9a9a14e2c2eb76e4bfbce024f1cd1771ae
Your Name <you@example.com>
2022-02-15 21:18:04 -0500

    Stub class and methods for Microphone Sound Level Circuit

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+
+################### MICROPHONE SOUND LEVEL CIRCUIT SECTION ################### 
+
+# Author: Developed and maintained by Andrew P. Mayer
+# Creation Date: 2/15/2022
+# Last Updated: 2/15/2022
+# License: MIT License 2022
+# Further Description:
+#   Stub function section for the CMEJ-9745-37-P Electric Condensor Microphone
+#   This section was written in VS Code and tested on a Raspberry Pi Zero
+
+# TODO:
+#       create stub functions and classes
+#       write stub functions and classes
+
+
+
+class micCircuit: 
+
+    def __init__(self):
+        pass;
+
+    def readAnalogVal(self):
+        pass;
+
+    def getA2C(self):
+        pass;
+
+
+
