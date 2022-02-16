a5627718bec42feeb4e33fadde14399255cb4f4f
Your Name <you@example.com>
2022-02-15 21:45:09 -0500

    Pushing micCircuit library to Github

---
+++ micCircuit.py
@@ -0,0 +1,35 @@
+################### MICROPHONE SOUND LEVEL CIRCUIT SECTION ################### 
+
+# Author: Developed and maintained by Andrew P. Mayer
+# Creation Date: 2/15/2022
+# Last Updated: 2/15/2022
+# License: MIT License 2022
+# Further Description:
+#   Stub function section for the CMEJ-9745-37-P Electric Condensor Microphone
+#   This section was written in VS Code and tested on a Raspberry Pi Zero
+
+# TODO:
+#       write stub functions and classes
+
+
+
+class micCircuit: 
+
+    # initializes micCircuit class
+    def __init__(self):
+        pass;
+
+    # reads in analog value and returns it
+    def readAnalogVal(self):
+        pass;
+
+    # converts given analog value to digital value
+    def atod(self, aVal):
+        pass;
+
+    # returns true if curr analog value is greater than threshold false otherwise
+    def trigger(self, thresholdVal):
+        pass;
+
+
+
