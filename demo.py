3c5eb8c1a663247ea060ed8ba072015eafea9302
Your Name <you@example.com>
2022-03-17 12:40:47 -0400

    Added LED Bar methods to demo.py (Issue #67)

--- demo.py
+++ demo.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     print("Average Digital Value: ", "{:.0f}".format(avg))
     print("Average Analog Value: ",  "{:.2f}".format(m.getAnalogVal(avg)), "V")
     trigVal = 2.5
-    # isTriggered = m.trigger(trigVal, tIntv)
-    # message = "Voltage is above threshold value..." if isTriggered else "Voltage is below threshold value..."
-    # print(message); 
+    isTriggered = m.trigger(trigVal, tIntv)
+    message = "Voltage is above threshold value..." if isTriggered else "Voltage is below threshold value..."
+    print(message); 
 
     #################### CODE TO TEST STEREO DECODER ##################
 
@@ -71,6 +71,20 @@ if __name__ == "__main__":
     #print("Oxygen Saturation Level (SPO2): ", oSatLevel)
     #temp = hrs.getTemp()
     #print("Temperature: ", temp)
+
+    ###################### CODE TO TEST LED Bar #######################
+
+    # initialize led bar object
+    lBar = infra.ledBar()
+    print("Testing LED Bar...")
+    # call and display led bar methods
+    print("Breathing in...")
+    lBar.breath_in()
+    print("Breathing out...")
+    lBar.breath_out()
+    
+
+
 
 
 
