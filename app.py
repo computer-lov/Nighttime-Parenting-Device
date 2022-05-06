0d974fe630c56417e86ad992d78313f887bacbd0
arongoldberg <89358150+arongoldberg@users.noreply.github.com>
2022-05-05 22:02:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def sendEmail(message):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port) as server:
         server.set_debuglevel(1)
-        server.starttls(context=context)
+        #server.starttls(context=context)
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
