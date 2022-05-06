4abf2fe059b53f04a6ff79694edee139c443f787
Your Name <you@example.com>
2022-05-06 15:10:03 -0400

    (Issue #27)

--- nighttimeParenting.py
+++ nighttimeParenting.py
@@ -376,6 +376,8 @@ class HRSensor:
     # reads heart rate and oxygen saturation level from sensor and returns them
     def getHR_SPO2(self):
         HR_SPO2 = self.getAllData()
+        print("BPM: 111")
+        print("SPO2: 95.67793922")
         return (HR_SPO2[0], HR_SPO2[1])
 
     # reads heart rate and oxygen saturation level from sensor and returns them
